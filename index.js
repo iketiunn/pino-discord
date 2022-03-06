e328f86f309286feabf0c7cc2bc0729395537b6f
ike <ike.chang@sideeffect.dev>
2022-03-06 12:41:18 +0800

    fix: catch and log error in pump, add validation on logBody.level

--- index.js
+++ index.js
@@ -14,11 +14,18 @@ pump(
   process.stdin,
   split(JSON.parse),
   through.obj(function (chunk, _enc, cb) {
+    if (isNaN(chunk.level)) {
+      throw new TypeError("Invalid pino log format!")
+    }
+
     if (opts.level && Number(chunk.level) >= Number(opts.level)) {
       // Send to hook
       post(url, makeBody(opts, chunk));
     }
 
     cb();
-  })
+  }),
+  function(err) {
+    if (err) console.error(err)
+  }
 );
